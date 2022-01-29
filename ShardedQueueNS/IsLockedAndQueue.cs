a7e6a994924d464614a8fb1a056e3310e9e840ba
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-01-30 02:38:16 +0300

    Moved `IsLockedAndQueue` to separate project, since it needs to be reused in `RunAtomicallyForMultipleResourceIdsAsyncProvider`

--- ShardedQueueNS/IsLockedAndQueue.cs
+++
@@ -1,9 +0,0 @@
-using SpinLockUtilNS;
-
-namespace ShardedQueueNS;
-
-internal class IsLockedAndQueue<T>
-{
-    internal readonly Queue<T> Queue = new();
-    internal int IsLocked = SpinLockUtil.False;
-}
